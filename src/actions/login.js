38b92574b40ebbcdfffc661ba803d288fcd06303
Ricardo Dantas <ricardodantas@gmail.com>
2017-05-16 20:10:15 -0300

    refatored to add axios

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,42 +1,47 @@
 import { createAction } from 'redux-actions'
+import {isEmpty} from 'lodash'
+import axios from 'axios'
 
 export const loginFetching = createAction('LOGIN_FETCHING')
 export const loginError    = createAction('LOGIN_ERROR')
 export const loginSuccess  = createAction('LOGIN_SUCCESS')
-export const logout  = createAction('LOGOUT', () => sessionStorage.removeItem('token'))
+export const logout  = createAction('LOGOUT', () => sessionStorage.clear())
 
 function storeToken(token) {
-  sessionStorage.setItem('token', token)
+  if (isEmpty(token)) {
+      throw new Error("Token is empty.")
+  }
 
-  return {
-    type: 'LOGIN_STORE_TOKEN',
+  sessionStorage.setItem('token', token)
+  return token
+}
+
+export async function fetchLogin (email, password) {
+  try {
+    return await axios.post('https://i2x-challenge.herokuapp.com/core/login/', {
+        email: email,
+        password: password
+      },{
+        headers: {
+          "content-type": "application/json"
+        }
+      })
+  } catch (e) {
+    return e.message
   }
 }
 
 export function login(email, password) {
-  return dispatch => {
+  return async (dispatch) => {
     dispatch(loginFetching())
-    return fetch('https://i2x-challenge.herokuapp.com/core/login/',{
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: new Headers(
-           {
-             "content-type": "application/json",
-          }
-        ),
-        body: JSON.stringify(
-          {
-            email: email,
-            password: password
-          }
-        )
-     })
-    .then((response) => response.ok ? response.json() : Promise.reject(response.text()))
-    .then((responseJson) => {
-      dispatch(storeToken(responseJson.token))
-      dispatch(loginSuccess(responseJson))
-    })
-    .catch((error) => dispatch(loginError(error.message)))
+    const response = await fetchLogin(email, password)
+    const {data} = response
+
+    if (!isEmpty(data) && !isEmpty(data.token)) {
+      storeToken(data.token)
+      dispatch(loginSuccess({data}))
+    } else {
+      dispatch(loginError({message: response}))
+    }
   }
 }
